903374ecf6089a70c2ee6fb9798ac1ccf815fbc0
araza6 <asadraza1999@hotmail.com>
2020-06-17 23:25:18 +0800

    updated files/folders for pypi upload

--- setup.py
+++ setup.py
@@ -1,16 +1,43 @@
-#!/usr/bin/env python
+"""A Python library to integrate automatic differentiation tools 
+such as Jax with QuTiP and related quantum software packages.
+"""
 
-from setuptools import setup, find_packages
+CLASSIFIERS = """\
+Intended Audience :: Science/Research
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering/Research
+Operating System :: MacOS
+Operating System :: POSIX :: Linux
+Operating System :: Unix
+Operating System :: Microsoft :: Windows
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 
-setup(
-    name="qgrad",
+NAME = "qgrad"
+AUTHOR = ("Asad Raza, Nathan Shammah, Shahnawaz Ahmed")
+AUTHOR_EMAIL = ("asadraza1999@hotmail.com, nathan.shammah@gmail.com, "
+                "shahnawaz.ahmed95@gmail.com")
+URL = "https://github.com/qgrad/qgrad"
+CLASSIFIERS = [_c for _c in CLASSIFIERS.split('\n') ]
+PLATFORMS = ["Linux", "Mac OSX", "Unix", "Windows"]
+
+setuptools.setup(
+    name = NAME,
     version="0.0.1",
-    description="Autodifferentiate quantum functions ",
-    author="Shahnawaz Ahmed, Asad Raza, Nathan Shammah",
-    author_email="shahnawaz.ahmed95@gmail.com",
-    url="https://github.com/qgrad/qgrad",
-    packages=find_packages(),
-    install_requires=["jaxlib", "numpy"],
-    python_requires=">=3.6",
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    description = __doc__,
+    long_description = __doc__,
+    long_description_content_type = "text/markdown",
+    url = URL,
+    packages = setuptools.find_packages(),
+    classifiers = CLASSIFIERS,
+    python_requires='>=3.6',
 )
+
