f16ae2149857c9ced71c416acf7a59a585e3d956
araza6 <asadraza1999@hotmail.com>
2020-06-18 17:19:58 +0800

    tests for fidelity

--- qgrad/qutip.py
+++ qgrad/qutip.py
@@ -20,7 +20,7 @@ def fidelity(a, b):
 
 def rot(params):
     """
-    Returns a unitary matrix describing rotation around Z-Y-Z axis
+    Returns a unitary matrix describing rotation around Z-Y-Z axis for a single qubit
 
     Args:
         params (`:obj:numpy.array`[float]): an array of three parameters defining the
