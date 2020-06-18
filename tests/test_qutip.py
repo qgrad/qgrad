6a1809c2a3deec0106350164f998770bb4da27ff
araza6 <asadraza1999@hotmail.com>
2020-06-18 17:19:58 +0800

    tests for fidelity

--- tests/test_qutip.py
+++ tests/test_qutip.py
@@ -11,13 +11,26 @@ def test_fidelity():
     """
     Tests the fidelity function and computation of its gradient
     """
-    ket1 = jnp.array([[1.0], [0]])
-    ket2 = jnp.array([[0.0], [1]])
+    ket0 = jnp.array([[1.0], [0]]) #represents |0>
+    ket1 = jnp.array([[0.0], [1]]) #represents |1>
+    ket_plus = 1/jnp.sqrt(2)*(ket0 + ket1) #represents |+>
+    ket_minus = 1/jnp.sqrt(2)*(ket0 - ket1) #represents |->
 
-    assert fidelity(ket1, ket2) == 0.0
-
-
+    assert fidelity(ket0, ket1) == 0.0
+    assert fidelity(ket0, ket0) == 1.0
+    assert fidelity(ket1, ket1) == 1.0
+    assert fidelity(ket_plus, ket_minus) == 0.0
+    assert fidelity(ket_plus, ket0) == 1.0 / jnp.sqrt(2)
+    assert fidelity(ket_plus, ket1) == 1.0 / jnp.sqrt(2)
+    assert fidelity(ket0, ket_minus) == 1.0 / jnp.sqrt(2)
+    assert fidelity(ket1, ket_minus) == - (1.0 / jnp.sqrt(2))
+  
 def test_rot():
     """
     Tests the rot function and computation of its gradient
     """
+    assert jnp.all(rot([0, 0, 0]) == jnp.identity(2, dtype = 'complex64')) 
+
+
+test_fidelity()
+test_rot()        
