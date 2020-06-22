7f817dfc8b34c4b9bf84e0b3ed3ab58d360e24df
araza6 <asadraza1999@hotmail.com>
2020-06-22 16:45:05 +0800

    annilhilation oper; Scipy Value Error(Last Value of index pointer lesss than)

--- qgrad/qutip.py
+++ qgrad/qutip.py
@@ -1,6 +1,8 @@
 """
 Implementation of some common quantum mechanics functions that work with Jax
 """
+from scipy.sparse import csr_matrix
+from jax.ops import index, index_update
 import jax.numpy as jnp
 
 
@@ -43,6 +45,7 @@ def rot(params):
         ]
     )
 
+
 def sigmax():
     r"""Returns a Pauli-X operator
     .. math:: \sigma_{x} = \begin{bmatrix} 0 & 1 \\ 1 & 0 \end{bmatrix}. 
@@ -54,7 +57,8 @@ def sigmax():
      [1. 0.]]
 
     """
-    return jnp.asarray([[0., 1.], [1., 0.]])
+    return jnp.asarray([[0.0, 1.0], [1.0, 0.0]])
+
 
 def sigmay():
     r"""Returns a Pauli-Y operator
@@ -67,7 +71,8 @@ def sigmay():
      [0.+1.j 0.+0.j]]
 
     """
-    return jnp.asarray([[0.+0.j, 0.-1.j], [0.+1.j, 0.+0.j]])
+    return jnp.asarray([[0.0 + 0.0j, 0.0 - 1.0j], [0.0 + 1.0j, 0.0 + 0.0j]])
+
 
 def sigmaz():
     r"""Returns a Pauli-Y operator
@@ -80,4 +85,30 @@ def sigmaz():
      [0. -1.]]
 
     """
-    return jnp.asarray([[1., 0.], [0., -1.]])
+    return jnp.asarray([[1.0, 0.0], [0.0, -1.0]])
+
+
+def destroy(N, full=False):
+    """Destruction (lowering or annihilation) operator.
+    
+    Args:
+        N (int): Dimension of Hilbert space.
+        full (bool): Returns a full matrix if `True` and Compressed Sparse Matrix if `False`  
+
+    Returns:
+         `obj:numpy.array`[complex]: Matrix representation for an N-dimensional annihilation operator
+
+    """
+    if not isinstance(N, (int, jnp.integer)):  # raise error if N not integer
+        raise ValueError("Hilbert space dimension must be integer value")
+    data = jnp.sqrt(jnp.arange(1, N, dtype="complex64"))
+    ind = jnp.arange(1, N, dtype=jnp.int32)
+    ptr = jnp.arange(N + 1, dtype=jnp.int32)
+    index_update(ptr, index[-1], N-1) #index_update mutates the jnp array in-place like numpy
+    return (
+        csr_matrix((data, ind, ptr), shape=(N, N))
+        if full is True
+        else csr_matrix((data, ind, ptr), shape=(N, N)).toarray()
+    )
+
+
