e45b755a690350ca4df28c95a662691c3cba8dcb
araza6 <asadraza1999@hotmail.com>
2020-06-26 17:23:12 +0800

    testing and batching

--- qgrad/optimizers/SGD.py
+++ qgrad/optimizers/SGD.py
@@ -5,11 +5,10 @@ import jax.numpy as jnp
 
 class SGD:
 
-    def __init__(self, lr=0.01, cost_func=None, params=None, \
-        argnums=[], batch_size=30):
-        self.batch_size = batch_size
-        self._grad = grad(cost_func, argnums=argnums)
+    def __init__(self, lr=0.01, cost_func=None, params=None):
+        self._grad = grad(cost_func)
         self.params = params
+        self.cost_func = cost_func
 
         if len(argnums) == 0:
             raise ValueError("Argnums cannot be empty")
@@ -18,27 +17,30 @@ class SGD:
             raise ValueError("Learning rate {} is negative. Choose a positive learning rate".format(lr))
 
 
-    def grad(self, val):
-        return jnp.asarray(self._grad(*val))
+    def grad(self, params, inputs, outputs):
+        return jnp.asarray(self._grad(params, inputs, outputs))
 
-    def mini_batchify(self, inputs, targets, batch_size):
-      # makes minibatches for mini batch GD
+    def _mini_batchify(self, inputs, outputs, batch_size):
+        for start_idx in range(0, inputs.shape[0] - batch_size + 1, batch_size):
+            yield inputs[start_idx : start_idx + batch_size], outputs[start_idx : start_idx + batch_size]
+
  
-    def train(self, max_iter=100, epochs=100, tol=1e-7, diff=1):
+    def train(self, inputs, outputs, batch_size=30, max_iter=100, epochs=100, tol=1e-7, diff=1):
         for epochs in range(epochs):
         _iter = max_iter
         _diff = diff
-        
+            for xbatch, ybatch in self._mini_batchify(inputs, outputs, batch_size):
+                self.step(params, xbatch, ybatch) 
+ 
         while jnp.all(_diff > tol) and _iter < max_iters:
             _diff = jnp.absolute(self.params - self.step(self.params))
             _iter += 1
        
         return self.params
-
         
-    def step(self, curr):
-        self.params = self.params - lr * grad(curr):
+    def step(self, params, inputs, outputs):
+        self.params = self.params - lr * self.grad(params, inputs, outputs):
         return self.params
 
-    def test(self, test_data):
-     # test with optimized params
+    def test(self, params, inputs, outputs):
+        return self.cost_func(params ,inputs, outputs)
