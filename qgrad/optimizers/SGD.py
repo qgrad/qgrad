ae996cff96a749bb3a0efe3a728730c9a121dd9c
araza6 <asadraza1999@hotmail.com>
2020-06-26 20:26:06 +0800

    class docstring

--- qgrad/optimizers/SGD.py
+++ qgrad/optimizers/SGD.py
@@ -4,10 +4,25 @@ from jax import grad
 import jax.numpy as jnp
 
 class SGD:
+    """Implements Stochastic Gradient Descent.
 
-    def __init__(self, lr=0.01, cost_func=None, params=None):
+    Training can be fully accomplished in one function call to `train`. 
+    Model can be tested with optimized paramters via `test`
+
+    Args:
+        lr (float): learning rate :math:`\alpha` in the routine 
+
+        .. math:: \theta^{t+1} = \theta^{t} - \alpha \nabla J(\theta^{t})
+
+        where :math:`J(\theta^{t})` is the value of the cost function at time step t.
+
+        cos_func (function): objective function to be minimized. `cost_func` takes the
+               arguments of params to be optimized, 2-D input array and a 1-D target 
+               array of outputs, in that order: `cost_func(params, inputs, outputs)`
+    """
+
+    def __init__(self, lr=0.01, cost_func=None):
         self._grad = grad(cost_func)
-        self.params = params
         self.cost_func = cost_func
 
         if len(argnums) == 0:
@@ -25,7 +40,7 @@ class SGD:
             yield inputs[start_idx : start_idx + batch_size], outputs[start_idx : start_idx + batch_size]
 
  
-    def train(self, inputs, outputs, batch_size=30, max_iter=100, epochs=100, tol=1e-7, diff=1):
+    def train(self, inputs, outputs, params, batch_size=30, max_iter=100, epochs=100, tol=1e-7, diff=1):
         for epochs in range(epochs):
             for xbatch, ybatch in self._mini_batchify(inputs, outputs, batch_size):
                 for x, y in zip(xbatch, ybatch):
