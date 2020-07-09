fixed SytaxError in rotation test

qgrad/tests/test_qutip.py
@@ -95,9 +95,9 @@ def test_fidelity_bounded_purepure(tol=1e-7):
 
 def test_rot():
     """Tests the rot function and computation of its gradient"""
-    ket0 = jnp.asarray([1, 0], dtype="complex64")
-    evo = jnp.dot(rot[0.5, 0.7, 0.8], ket0)
-    back_evo = jnp.dot(rot[0.5, 0.7, 0.8], evo)
+    ket0 = jnp.array([1, 0], dtype=jnp.complex64)
+    evo = jnp.dot(rot([0.5, 0.7, 0.8]), ket0)
+    back_evo = jnp.dot(rot([0.5, 0.7, 0.8]), evo)
 
     assert jnp.all(rot([0, 0, 0]) == jnp.identity(2, dtype="complex64"))
     assert not jnp.all(jnp.equal(evo, back_evo))